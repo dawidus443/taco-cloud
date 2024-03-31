81290276c2dff55cd7f4e3d9b7126a2dd4d7d2d3
dawidus443 <dawidbadora@gmail.com>
2024-04-01 00:58:34 +0200

    Taco validation

--- src/main/java/com/example/tacocloud/model/Taco.java
+++ src/main/java/com/example/tacocloud/model/Taco.java
@@ -1,5 +1,7 @@
 package com.example.tacocloud.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,10 @@ import java.util.List;
 //@AllArgsConstructor
 //@NoArgsConstructor
 public class Taco {
+
+    @NotNull
+    @Size(min = 5, message = "Providing your name is mandatory.")
     private String name;
+    @Size(min = 1, message = "You have choose at least 1 ingredient.")
     private List<String> ingredients;
 }
